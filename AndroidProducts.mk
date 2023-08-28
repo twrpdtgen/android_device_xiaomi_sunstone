bf94a13c5e44a00d711f66a8cbdaba5a3088fe13
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-08-28 18:35:41 +0200

    sunstone: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 22101317C Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sunstone.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sunstone-user \
+    omni_sunstone-userdebug \
+    omni_sunstone-eng
